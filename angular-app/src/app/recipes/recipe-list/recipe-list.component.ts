3e529063bb5744cfe39d8015eee4948ce7024621
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-02-14 22:28:44 +0530

    updates

--- angular-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK1V9y4aAUZlpdamdasLFMxYjMbi8rEzcAZg&usqp=CAU')];
 }
