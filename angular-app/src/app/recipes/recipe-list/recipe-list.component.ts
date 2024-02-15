2c5708bc6006e1c9bbd43904aca9daecef84fc56
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-02-16 01:23:38 +0530

    updates: changes in UI

--- angular-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,5 +7,5 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-  recipes: Recipe[] = [new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK1V9y4aAUZlpdamdasLFMxYjMbi8rEzcAZg&usqp=CAU')];
+  recipes: Recipe[] = [new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK1V9y4aAUZlpdamdasLFMxYjMbi8rEzcAZg&usqp=CAU'), new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK1V9y4aAUZlpdamdasLFMxYjMbi8rEzcAZg&usqp=CAU')];
 }
