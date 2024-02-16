74299ba825209646dde0a2091cdac8aa07d6cbed
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-02-16 12:04:49 +0530

    changes

--- cmp-databinding-app/src/app/app.component.ts
+++ cmp-databinding-app/src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+interface ServerElements {
+  type: string;
+  name: string;
+  content: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cmp-databinding-app';
+  serverElements: ServerElements[] = [];
+  serverName = '';
+  serverContent = '';
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.serverName,
+      content: this.serverContent
+    })
+  }
+
+  onAddServerBlueprint() {
+     this.serverElements.push({
+      type: 'blueprint',
+      name: this.serverName,
+      content: this.serverContent
+     })
+  }
 }
