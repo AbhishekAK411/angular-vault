43fa6bd350992f3abad856ec3947d2ce9ffecf1a
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-02-17 23:25:29 +0530

    updates

--- cmp-databinding-app/src/app/server-element/server-element.component.ts
+++ cmp-databinding-app/src/app/server-element/server-element.component.ts
@@ -1,11 +1,11 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
 })
-export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   @Input('srvElement') element!: { type: string; name: string; content: string; };
 
   constructor() {
@@ -38,5 +38,9 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
 
   ngAfterViewChecked(): void {
     console.log("ngAfterViewChecked called.");
+  }
+
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy called.");
   }
 }
