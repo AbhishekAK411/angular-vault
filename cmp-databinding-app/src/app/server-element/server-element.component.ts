2d5a9dda0f907f8fd29a3d7e3a6d7b2743f39598
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-02-17 20:22:51 +0530

    updates: lifecycle hooks

--- cmp-databinding-app/src/app/server-element/server-element.component.ts
+++ cmp-databinding-app/src/app/server-element/server-element.component.ts
@@ -1,12 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
 })
-export class ServerElementComponent {
+export class ServerElementComponent implements OnInit, OnChanges {
   @Input('srvElement') element!: { type: string; name: string; content: string; };
 
+  constructor() {
+    console.log("constructor called.");
+  }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log("onInit called.");
+  }
 }
