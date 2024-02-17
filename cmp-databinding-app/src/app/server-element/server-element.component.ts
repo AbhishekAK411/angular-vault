d40ece0c53233abf8fe1d434e2ba9303b7aadf40
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-02-17 23:23:37 +0530

    updates: lifecycle hooks checked

--- cmp-databinding-app/src/app/server-element/server-element.component.ts
+++ cmp-databinding-app/src/app/server-element/server-element.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
 })
-export class ServerElementComponent implements OnInit, OnChanges {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
   @Input('srvElement') element!: { type: string; name: string; content: string; };
 
   constructor() {
@@ -18,5 +18,25 @@ export class ServerElementComponent implements OnInit, OnChanges {
 
   ngOnInit() {
     console.log("onInit called.");
+  }
+
+  ngDoCheck(): void {
+    console.log("ngDoCheck called.");
+  }
+
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit called.");
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked called.");
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit called.");
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked called.");
   }
 }
