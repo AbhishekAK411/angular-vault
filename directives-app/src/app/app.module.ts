1fd1f7841a3385ff5be753128fb1a2ee4ec4857c
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-02-19 02:30:17 +0530

    updates: basic-highlight.directive.ts, app.module.ts changes

--- directives-app/src/app/app.module.ts
+++ directives-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { BasicHighLightDirective } from './basic-highlight/basic-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighLightDirective
   ],
   imports: [
     BrowserModule,
