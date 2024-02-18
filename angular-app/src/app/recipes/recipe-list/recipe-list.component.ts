80ad5d69f3cb9b3b542ef501047294a8c8fb4ea0
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-02-18 19:39:28 +0530

    changes

--- angular-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,5 +7,10 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK1V9y4aAUZlpdamdasLFMxYjMbi8rEzcAZg&usqp=CAU'), new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK1V9y4aAUZlpdamdasLFMxYjMbi8rEzcAZg&usqp=CAU')];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
