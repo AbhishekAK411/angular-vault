f8743528d62f35ff8eba0dc242e7c733a303bacd
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-02-19 10:51:17 +0530

    updates: better-highlight, renderer

--- directives-app/src/app/app.module.ts
+++ directives-app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BasicHighLightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighLightDirective
+    BasicHighLightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
