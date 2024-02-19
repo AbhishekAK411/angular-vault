3ee4abc4a620385fb21ae9bd9252770e06bb3bec
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-02-19 20:03:26 +0530

    update: custom directive toggle dropdown

---
+++ angular-app/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, ElementRef, HostBinding, HostListener, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]',
+})
+export class DropdownDirective implements OnInit {
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
+  
+    constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+}
