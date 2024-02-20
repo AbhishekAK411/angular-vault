d1a540d3b08097c0d2184dd3deae3b689e7d4c2e
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-02-20 14:16:41 +0530

    update: @injectable -> accounts.service.ts, root provider in app.module.ts along with loggingService

--- services-depinjection-app/src/app/new-account/new-account.component.ts
+++ services-depinjection-app/src/app/new-account/new-account.component.ts
@@ -6,17 +6,19 @@ import { AccountsService } from '../accounts.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
 
   constructor(
     private loggingService: LoggingService,
     private accountService: AccountsService
-  ) {}
+  ) {
+    this.accountService.statusUpdated.subscribe((status: string) => alert('New Status: ' + status));
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
   }
 }
