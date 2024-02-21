e794c18d3de99e4e923896a248b3fea668e76937
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-02-21 15:10:23 +0530

    implemented adding ingredients from recipe to shopping list using a service

--- angular-app/src/app/recipes/recipe.service.ts
+++ angular-app/src/app/recipes/recipe.service.ts
@@ -1,5 +1,7 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
@@ -9,16 +11,30 @@ export class RecipeService {
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK1V9y4aAUZlpdamdasLFMxYjMbi8rEzcAZg&usqp=CAU'
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK1V9y4aAUZlpdamdasLFMxYjMbi8rEzcAZg&usqp=CAU',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
     ),
     new Recipe(
-      'A Test Recipe',
+      'Another Test Recipe',
       'This is simply a test',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK1V9y4aAUZlpdamdasLFMxYjMbi8rEzcAZg&usqp=CAU'
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK1V9y4aAUZlpdamdasLFMxYjMbi8rEzcAZg&usqp=CAU',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]
     ),
   ];
 
+  constructor(private slService: ShoppingListService) {}
+
   getRecipe() {
     return this.recipes.slice();
+  }
+
+  addIngredientToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
