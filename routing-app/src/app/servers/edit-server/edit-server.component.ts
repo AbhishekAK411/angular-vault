aa044fd6b6c326d646413c7388c5364d412ece3a
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-02-28 13:06:46 +0530

    updates: query parameters and using queryParams observable

--- routing-app/src/app/servers/edit-server/edit-server.component.ts
+++ routing-app/src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,6 +12,7 @@ export class EditServerComponent implements OnInit {
   server!: { id: number; name: string; status: string };
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(
     private serversService: ServersService,
@@ -19,11 +20,13 @@ export class EditServerComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    console.log(this.route.snapshot.queryParams); //! snapshots -> only gets loaded first time component is rendered.
-    console.log(this.route.snapshot.fragment); //! snapshots -> only gets loaded first time component is rendered.
+    // console.log(this.route.snapshot.queryParams); //! snapshots -> only gets loaded first time component is rendered.
+    // console.log(this.route.snapshot.fragment); //! snapshots -> only gets loaded first time component is rendered.
 
-    this.route.queryParams.subscribe(); //! Observable-> queryParams
-    this.route.fragment.subscribe(); //! Observable-> fragment 
+    this.route.queryParams.subscribe((queryParams: Params) => {
+      this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+    }); //! Observable-> queryParams
+    this.route.fragment.subscribe(); //! Observable-> fragment
 
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
