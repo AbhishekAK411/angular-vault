19bcd882ef7e096a0a9e2f1cf32f20634b798375
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-02-28 17:22:40 +0530

    updates: authentication service->dummy

--- routing-app/src/app/auth.service.ts
+++ routing-app/src/app/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class AuthService {
-    loggedIn: boolean = false;
+    private loggedIn: boolean = false;
 
     isAuthenticated() {
         const promise = new Promise(
