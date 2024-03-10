f7112914f5f54cdb0a3141770c022acf9f80305a
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-03-10 12:18:45 +0530

    updates: form TD assignment

--- forms-TD-assignment/src/app/app.module.ts
+++ forms-TD-assignment/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import {DropdownDirective} from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
