3a78e78f7806c4ae10bd13748f4fffbed8519303
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-03-10 18:15:50 +0530

    template-driven forms assignment complete

--- forms-TD-assignment/src/app/app.component.ts
+++ forms-TD-assignment/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') testForm!: NgForm;
+  public subscriptionMode: string[] = ['Basic', 'Advanced', 'Pro'];
+  public formSubmitted: boolean = false;
+  public selectedSubscription: string = 'Advanced'
+  user = {
+    email: "",
+    subscription: "",
+    password: ""
+  }
 
   onSubmit(): void {
-
+    this.formSubmitted = true;
+    this.user.email = this.testForm.value.userData.email;
+    this.user.subscription = this.testForm.value.userData.subscription;
+    this.user.password = this.testForm.value.userData.password;
+    console.log(this.user);
+    this.testForm.reset();
   }
 }
