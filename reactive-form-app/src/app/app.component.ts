f4f3ee86c9e52404d10b85c66e6d843de55c0f31
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-03-11 02:13:32 +0530

    updates: formGroupControl and nesting and creating FormGroups inside FormGroups

--- reactive-form-app/src/app/app.component.ts
+++ reactive-form-app/src/app/app.component.ts
@@ -7,13 +7,15 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  genders = ['male', 'female'];
-  signupForm!: FormGroup;
+  public genders = ['male', 'female'];
+  public signupForm!: FormGroup;
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
-      'username': new FormControl(null, Validators.required),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
       'gender': new FormControl('male')
     })
   }
